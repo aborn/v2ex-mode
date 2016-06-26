96e628191f94968cdae354b292b18f19d9a8416c
Aborn Jiang <loveaborn@foxmail.com>
2016-06-26 13:11:20 +0800

    add copyinfo

--- v2ex-mode.el
+++ v2ex-mode.el
@@ -9,6 +9,8 @@
 ;; Homepage: https://github.com/aborn/v2ex-mode
 ;; URL: https://github.com/aborn/v2ex-mode
 
+;; This file is NOT part of GNU Emacs.
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
