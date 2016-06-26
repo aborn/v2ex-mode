a664482f96f4ced021654bd2f7d9c81394f90ee0
Aborn Jiang <loveaborn@foxmail.com>
2016-06-26 17:08:42 +0800

    version 0.1, ready to release

--- v2ex-mode.el
+++ v2ex-mode.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2016 Aborn Jiang
 
 ;; Author: Aborn Jiang <aborn.jiang@gmail.com>
-;; Version: 1.0
+;; Version: 0.1
 ;; Package-Requires: ((cl-lib "0.5") (request "0.2"))
 ;; Keywords: v2ex, v2ex.com
 ;; Homepage: https://github.com/aborn/v2ex-mode
